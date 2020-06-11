783e4e150afbd30742c59e982b3e86ccf703d0fe
Andreas Schumm <andreas@schumm-net.de>
2020-06-11 11:13:23 +0200

    Added ZipCodeConverter

---
+++ src/main/java/de/bruxxen/MyGourmet/Converter/ZipCodeConverter.java
@@ -0,0 +1,28 @@
+package de.bruxxen.MyGourmet.Converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter("de.bruxxen.MyGourmet.Converter.ZipCodeConverter")
+public class ZipCodeConverter implements Converter {
+
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+		
+		if(value != null) {
+			for (int i=0; i < value.length(); i++) {
+				if (!Character.isDigit(value.charAt(i))) {
+					value = value.substring(0, i) + value.substring(i + 1);
+					i--;
+				}
+			}
+		}		
+		return value;
+	}
+
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+		return value.toString();
+	}
+
+}
