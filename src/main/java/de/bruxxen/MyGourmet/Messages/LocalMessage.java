889e109e7d40e22329964ebe729bf3be5f5eedc4
Andreas Schumm <andreas@schumm-net.de>
2020-06-12 13:10:17 +0200

    Added function to switch localization

--- src/main/java/de/bruxxen/MyGourmet/Messages/LocalMessage.java
+++ src/main/java/de/bruxxen/MyGourmet/Messages/LocalMessage.java
@@ -5,8 +5,11 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import javax.faces.view.Location;
 
+@ManagedBean
 public class LocalMessage {
 
 	public static FacesMessage getFacesMessage(FacesContext context, FacesMessage.Severity severity, String msgKey, Object...args) {
@@ -19,5 +22,19 @@ public class LocalMessage {
 			msg = format.format(args);
 		}
 		return new FacesMessage(severity, msg, null);
+	}
+	
+	public static void changeLocal() {
+		String newLocale = getOppositeLocale();
+		FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale(newLocale));
+	}
+	
+	public static String getCurrentLocale() {
+		return FacesContext.getCurrentInstance().getViewRoot().getLocale().toString();
+	}
+	public static String getOppositeLocale() {
+		String current = getCurrentLocale();
+		if (current.equalsIgnoreCase("de"))return "en";
+		else return "de";
 	}
 }
