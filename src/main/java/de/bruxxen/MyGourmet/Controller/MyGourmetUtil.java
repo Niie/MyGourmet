2c26f6367364763645c0d2372625d499c5513962
Andreas Schumm <andreas@schumm-net.de>
2020-06-16 12:57:41 +0200

    Added mainTemplate implemented mainTemplate for showCustomer and editCustomer Added 2 backup datas for the overwriten files CSS changes

--- src/main/java/de/bruxxen/MyGourmet/Controller/MyGourmetUtil.java
+++ src/main/java/de/bruxxen/MyGourmet/Controller/MyGourmetUtil.java
@@ -5,13 +5,19 @@ import java.util.Date;
 
 public class MyGourmetUtil {
 	public static int getAge(Date birthday) {
-		Calendar birthCal = Calendar.getInstance();
-		birthCal.setTime(birthday);
-		Calendar today = Calendar.getInstance();
+		if(birthday != null) {
+			Calendar birthCal = Calendar.getInstance();
+			birthCal.setTime(birthday);
+			Calendar today = Calendar.getInstance();
+			
+			int age = today.get(Calendar.YEAR) - birthCal.get(Calendar.YEAR);
+			if(today.get(Calendar.DAY_OF_YEAR) < birthCal.get(Calendar.DAY_OF_YEAR)) age--;
+			return age;
+		}
+		else {
+			return 0;
+		}
 		
-		int age = today.get(Calendar.YEAR) - birthCal.get(Calendar.YEAR);
-		if(today.get(Calendar.DAY_OF_YEAR) < birthCal.get(Calendar.DAY_OF_YEAR)) age--;
-		return age;
 	}
 
 }
