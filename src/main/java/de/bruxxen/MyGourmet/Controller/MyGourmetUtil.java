57e2b27f668ebdac7cb84e492c023b1bf71b549f
Andreas Schumm <andreas@schumm-net.de>
2020-06-16 11:14:37 +0200

    Added own taglib with getAge func and ZipConverter

---
+++ src/main/java/de/bruxxen/MyGourmet/Controller/MyGourmetUtil.java
@@ -0,0 +1,17 @@
+package de.bruxxen.MyGourmet.Controller;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class MyGourmetUtil {
+	public static int getAge(Date birthday) {
+		Calendar birthCal = Calendar.getInstance();
+		birthCal.setTime(birthday);
+		Calendar today = Calendar.getInstance();
+		
+		int age = today.get(Calendar.YEAR) - birthCal.get(Calendar.YEAR);
+		if(today.get(Calendar.DAY_OF_YEAR) < birthCal.get(Calendar.DAY_OF_YEAR)) age--;
+		return age;
+	}
+
+}
